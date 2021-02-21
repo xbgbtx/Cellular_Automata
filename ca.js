bf934668cb78d560169b1082b1ec06c1add002c5
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2021-02-21 23:26:34 +0000

    Switch to p5js

--- ca.js
+++ ca.js
@@ -1,79 +1,35 @@
-function page_loaded () 
+let width = 800;
+let height = 600;
+
+let buffer_0, buffer_1;
+
+function setup ()
 {
-    console.log ( "Cellular Automata starting..." );
-
-    let pixi_app = init_pixi ();
-
-    let ca_data = init_ca_data ( 800, 800 );
-
-    pixi_app.stage.addChild ( ca_data.sprite );
-
-    pixi_app.ticker.add ( () =>
-    {
-        main_loop ( pixi_app, ca_data );
-    });
-}
-
-function init_pixi ()
-{
-    let type="WebGL";
+    createCanvas ( width, height );
     
-    if ( !PIXI.utils.isWebGLSupported () )
-    {
-        type = "canvas";
-    }
+    buffer_0 = new Uint8ClampedArray ( width * height * 4 );
+    buffer_1 = new Uint8ClampedArray ( width * height * 4 );
+}
 
-    PIXI.utils.sayHello ( type );
+function draw () 
+{
+    loadPixels ();
 
-    pixi_app = new PIXI.Application (
+    buffer_0.forEach ( ( val, idx ) =>
     {
-        width  : 800,
-        height : 800
+        buffer_1 [ idx ] = Math.floor ( Math.random () * 255 );
     });
 
-    document.getElementById ( "canvas_div" ).appendChild ( pixi_app.view );
+    let tmp = buffer_0;
+    buffer_0 = buffer_1;
+    buffer_1 = tmp;
 
-
-    return pixi_app;
-}
-
-function init_ca_data ( width, height ) {
-    let ca_data = {
-        width       : width,
-        height      : height,
-        buffer      : undefined,
-        texture     : undefined,
-        sprite      : undefined
-    }
-
-    ca_data.buffer = new Uint8ClampedArray ( width * height * 3 );
-
-    ca_data.texture = make_sim_texture ( ca_data );
-
-    ca_data.sprite = new PIXI.Sprite ( ca_data.texture );
-
-    return ca_data;
-}
-
-function make_sim_texture ( ca_data ) 
-{
-    if ( ca_data.texture != undefined )
+    pixels.forEach ( ( val, idx, arr ) => 
     {
-        ca_data.texture.destroy ( true );
-    }
+        pixels [ idx ] = buffer_0 [ idx ];
+    });
 
-    return PIXI.Texture.fromBuffer ( 
-        ca_data.buffer, ca_data.width, ca_data.height, 
-        { format : PIXI.FORMATS.RGB } );
-}
-
-function main_loop ( pixi_app, ca_data ) 
-{
-    ca_data.buffer = ca_data.buffer.map ( 
-        ( val, idx, arr ) => 
-        {
-            return Math.floor ( Math.random () * 255 );
-        });
-
-    ca_data.sprite.texture = make_sim_texture ( ca_data );
+    updatePixels ();
+    
+    //noLoop ();
 }
